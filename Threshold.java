f961360cdcd7aa9e4c22246595cddc6f457dc8c6
251575 Łuczak Klaudia <251575@edu.p.lodz.pl>
2025-12-05 15:07:36 +0100

    Thresholds

--- Threshold.java
+++ Threshold.java
@@ -1,2 +1,57 @@
 public class Threshold {
+    private Integer thresholdId;
+    private String thresholdType;
+    private Float valueInfo;
+    private Float valueWarning;
+    private Float valueEmergency;
+
+    public Threshold(Integer thresholdId, String thresholdType, Float valueInfo, Float valueWarning, Float valueEmergency) {
+        this.thresholdId = thresholdId;
+        this.thresholdType = thresholdType;
+        this.valueInfo = valueInfo;
+        this.valueWarning = valueWarning;
+        this.valueEmergency = valueEmergency;
+    }
+
+    public Integer getThresholdId() {
+        return thresholdId;
+    }
+
+    public String getThresholdType() {
+        return thresholdType;
+    }
+
+    public void setThresholdType(String thresholdType) {
+        this.thresholdType = thresholdType;
+    }
+
+    public Float getValueInfo() {
+        return valueInfo;
+    }
+
+    public void setValueInfo(Float valueInfo) {
+        this.valueInfo = valueInfo;
+    }
+
+    public Float getValueWarning() {
+        return valueWarning;
+    }
+
+    public void setValueWarning(Float valueWarning) {
+        this.valueWarning = valueWarning;
+    }
+
+    public Float getValueEmergency() {
+        return valueEmergency;
+    }
+
+    public void setValueEmergency(Float valueEmergency) {
+        this.valueEmergency = valueEmergency;
+    }
+
+    public void setValues(Float valueInfo, Float valueWarning, Float valueEmergency) {
+        this.valueInfo = valueInfo;
+        this.valueWarning = valueWarning;
+        this.valueEmergency = valueEmergency;
+    }
 }
--- ThresholdRepository.java
+++ ThresholdRepository.java
@@ -1,2 +1,43 @@
+import java.util.List;
+
 public class ThresholdRepository {
+    private List<Threshold> thresholds;
+
+    public ThresholdRepository() {
+    }
+
+    public List<Threshold> getAllThresholds() {
+        return thresholds;
+    }
+
+    public Threshold getThresholdById(int id) {
+        for (Threshold t : thresholds) {
+            if (t.getThresholdId() == id) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public Boolean addThreshold(Threshold threshold) {
+        try {
+        thresholds.add(threshold);}
+        catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean deleteThreshold(int id) {
+        for (Threshold t : thresholds) {
+            if (t.getThresholdId() == id) {
+                try {thresholds.remove(t);}
+                catch (Exception e) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
 }
