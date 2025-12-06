3d112684385e2645cbff6f1ead31f9f63db1d4b8
hdudek <haniadudek05@gmail.com>
2025-12-06 20:55:57 +0100

    dodanie pozostałych klas

---
+++ AutomationPlan.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutomationPlan {
+    private Integer id;
+    private String name;
+    private List<AutomationRule> rules = new ArrayList<>();
+}
---
+++ AutomationRule.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AutomationRule {
+    private Integer deviceId;
+    private Map<String, Float> states = new HashMap<>();
+    private String timeWindow;
+}
