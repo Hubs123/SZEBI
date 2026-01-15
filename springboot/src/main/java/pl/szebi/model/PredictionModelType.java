51a8ceeaf08194ff50a3433c6504887b301ca317
251631 Smyczyński Jakub <251631@edu.p.lodz.pl>
2026-01-15 19:42:30 +0100

    środowisko do testowania integracji dwóch modułów

--- springboot/src/main/java/pl/szebi/model/PredictionModelType.java
+++
@@ -1,8 +0,0 @@
-package pl.szebi.model;
-
-public enum PredictionModelType {
-    SIMPLE_AVG,
-    MOVING_AVG,
-    LINEAR_TREND
-}
-
--- uruchom_fastapi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Skrypt do uruchomienia FastAPI
-cd python_module/src
-python3 -m app.main
